4c7fd05fd7284a9f1f7fc9e67b210be32fec269a
CHAVES BOLADEZA <robson.brant@escola.pr.gov.br>
2026-05-12 10:30:29 -0300

    Add files via upload

--- aula02.js
+++ aula02.js
@@ -1,3 +1,29 @@
     //DEFINIÇÃO DE VARIÁVEIS
 
- let pessoa 
+ let pessoa = "Nome da pessoa";
+ let idadePessooa = 30;
+ let alturaPessoa =1.93;
+ let statusPessoa = true;
+ let salaruiPessoa = 1500;
+ let salarioBonus = 100;
+
+ let salarioTotal = salarioBonus + salarioPessoa;
+
+ console.log( pessoa);
+ console.log( idadePessoa );
+ console.log( alturaPessoa );
+ console.log( statusPessoa);
+ console.log( salarioPessoa );
+ console.log( salarioTotal );
+
+
+ //UNINDO TODAS AS INFORMAÇÔES EM UM TEXTO:
+ console.log(
+    'O nme digitado foi: '+pessoa+
+    ' A idade digitada foi:'+idadePessoa+
+    ' a altura digitada foi:'+alturaPessoa+
+    ' o status da pessoa é: '+statusPessoa+
+    ' o salário da pessoa é: '+salarioPesoa+
+    ' e o salario bonus é: '+salarioBonus+
+    ' e o salario total é: '+salarioTotal+ ''
+ )

9daae0b5c3ece1839519fc2b93aaa160a4c6ef7d
CHAVES BOLADEZA <robson.brant@escola.pr.gov.br>
2026-05-18 09:22:15 -0300

    Add files via upload

---
+++ aula03.js
@@ -0,0 +1,20 @@
+let nome = "Robson";
+
+if (nome === "Robson ") {
+    console.log("Você digitou o nome correto");
+}else{
+    console.log("Você digitou o nome errado...");
+}
+
+let idade = 16;
+
+if (idade <= 18) {
+    console.log("Você é praticamente adulto...")
+}else if (idade > 18 ){
+    console.log("Você ainda é de menor...")
+}else {
+    console.log("Você é adulto...")
+}
+
+//FAÇA UMA VEREFICAÇÃO COM IF E ELSE
+//DE UMA PESSOA SE ELA PODE OU NÃO VOTAR
